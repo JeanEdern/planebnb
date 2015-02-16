eaf5616945f0f9b2007e2ba23ec2f538e2e40773
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-16 18:18:30 +0100

    user and plane validation

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,6 +1,22 @@
 class Plane < ActiveRecord::Base
   belongs_to :user
 
+  # validation user id (should unique and present)
+  validates :user_id, presence: true, uniqueness: true
+
+  # validation description (should be present)
+  validates :description, presence: true
+
+  # validation seat (should be present and should >0 and <20)
+  validates_numericality_of :seat, :on => :create
+  validates :seat, presence: true, inclusion: { in: (1..20).to_a, allow_nil: false }
+
+  # validation aeroclub (should be present)
+  validates :aeroclub, presence: true
+
+  # validation available (par défaut true)
+  validates :available, inclusion: { in: [true, false] }
+
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
 
