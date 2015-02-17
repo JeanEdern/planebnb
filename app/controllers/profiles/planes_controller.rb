f23a639e554a599cfad3ba8f4e4882c136b2f9e3
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-17 17:13:03 +0100

    planes method create and show

--- app/controllers/profiles/planes_controller.rb
+++ app/controllers/profiles/planes_controller.rb
@@ -1,28 +1,56 @@
 module Profiles
   class PlanesController < ApplicationController
-
+    before_action :find_user
     # current_user.planes...
 
     def index
+      @planes = @user.planes
     end
 
     def show
-
+      @plane = @user.planes.find(params[:id])
     end
 
     def new
+      @plane = @user.planes.build
+      @plane.available = true
     end
 
     def create
+      @plane = @user.planes.build(planes_params)
+      @plane.available = true
+      if @plane.save
+        redirect_to profile_plane_path(@plane)
+      else
+        raise
+        render :new
+      end
     end
 
     def edit
+      @plane = @user.planes.build(planes_params)
     end
 
-    def update
-    end
+    # def update
+    #   @plane = @user.planes.build(planes_params)
+    #   if
+
+    #   else
+
+    #   end
+    # end
 
     def destroy
     end
+
+   private
+
+  def planes_params
+    params.require(:plane).permit(:description, :seat, :aeroclub, :available, :created_at, :updated_at, :price, :picture)
+  end
+
+  def find_user
+     @user = current_user
+   end
   end
 end
