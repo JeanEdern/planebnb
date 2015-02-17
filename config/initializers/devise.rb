8cb8708c8c657d4a9b1428fa60a27385482ceb15
ilias hicham <ilias.hicham@gadz.org>
2015-02-17 16:47:45 +0100

    facebook connect added

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,9 +3,7 @@
 Devise.setup do |config|
 
 
-  Devise.setup do |config|
-    config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"], scope: 'email'
-  end
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"], scope: 'email', image_size: 'large',
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
