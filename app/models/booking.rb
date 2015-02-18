5480270020518b01cc6ee4458c2ffb14a8942b98
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-18 11:57:48 +0100

    add validations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,9 @@ class Booking < ActiveRecord::Base
   belongs_to :user
 
   # to do add validation
+  validates :user_id, presence: true
+  validates :plane_id, presence: true
+  validates :people, presence: true
+  validates :check_in, presence: true
+  validates :check_out, presence: true
 end
