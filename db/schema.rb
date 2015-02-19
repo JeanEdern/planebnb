1aab466835bbd8e8786c09a837930425d23f8c6d
antoineodier <odier@hab.de>
2015-02-19 12:45:16 +0100

    fin_status_reservations_utilisateur

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219103702) do
+ActiveRecord::Schema.define(version: 20150219113212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150219103702) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "status"
   end
 
   add_index "bookings", ["plane_id"], name: "index_bookings_on_plane_id", using: :btree
