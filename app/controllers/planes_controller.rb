cbd8da77df193ab88997dca2acd06817c246941b
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-20 09:27:53 +0100

    gmap marker done

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -14,9 +14,9 @@ class PlanesController < ApplicationController
       marker.lat plane.latitude
       marker.lng plane.longitude
       marker.picture({
-        url: "https://s3-eu-west-1.amazonaws.com/planebnb/plane_logo.svg?X-Amz-Date=20150219T194137Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=65494e793ca0210168a56d0d1a3f16211f38cf1b2754814158e8a5214b253b7f&X-Amz-Credential=ASIAI3TRXIUKKAFGUVFA/20150219/eu-west-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=AQoDYXdzEGUakALXn6C5jwJ/RSrHUBTWswGSAkFcd%2BT9G5hmiZw63P1ECxq75zfng5hB2DXd7cU%2B95fyL66%2BjPvaz6YV8RacfvqnefdsNX3mw6uCkBLptJr0mTwGDVu2Mtgyi07ahbuJzs//Ggy3Hg%2B3fDRSi8D52Bl1XZJmGOOHyAHZMnMQHOLJuEUECLDBYpsYJjmZuK/1ynyXHKnnU0BRzum7dG8iAtx2zp3VOGG3biqJ1xon5ddw24TJ8Eqag7u6%2BkZJoG5/9KQoVJUdw1TQabaR4MFG9TvoV5pC4gHUn3BmuP7fx%2BPHT4RFjSf3E6%2BBP7vPRBWc5ld3OgXXzKHYIRkcIXahwtpI/DStBvnuypwD176xiyA%2BsCD97pinBQ%3D%3D",
-        width: 32,
-        height: 32})
+        url: "https://s3-eu-west-1.amazonaws.com/planebnb/development/noun_4885_cc.svg?X-Amz-Date=20150220T082637Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=e9a6b4f028d24edccdc54eb5545c672b793df36cdf595884bd92d7f8d2c6d3ea&X-Amz-Credential=ASIAJIAWPMGEGLDR7WZQ/20150220/eu-west-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=AQoDYXdzEHEakAJJCBWjcPanmbMG%2BNngj0yTBGvyRLz9gYFFqkAdxs9wEd2ZKPgrjkWTJj/e%2BW0N5PXm6gbkNHKIh3wBBM7yG%2B5ORbr62TqbmbcBNXX40uhfpCJSmOpX%2Bupgtc/IZTJJVZ92QeBwGzlAmPnBkYre7b6Zw4UpAT3/BR0gEYjcUBhGA6XO7cBRtAIUeLh1YbWN1oIFxWp8q2wwpo47XD6ZyJ4E7p12FtFY/o0Sm7EwpwoQLzkP6HCJ1sM%2BGFRQmR2MO6WKRey5LEreoWMKIZD/WRLwycsi7LZ011pDxP6Dx1mgjNEmCqpBT5pMVsYgaqXEHF8JXeEzZiAGOkwLU5tA3Fwsm%2BHNWROVkiFPeWTr4AbwCiC/15unBQ%3D%3D",
+        width: 80,
+        height: 80})
     end
   end
 
