c2fa310bb6bcccd21ce0a18b04f4ea2d4fe279b6
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-20 14:03:56 +0100

    marker done

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -14,9 +14,10 @@ class PlanesController < ApplicationController
       marker.lat plane.latitude
       marker.lng plane.longitude
       marker.picture({
-        url: "https://s3-eu-west-1.amazonaws.com/planebnb/development/noun_4885_cc.svg?X-Amz-Date=20150220T082637Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=e9a6b4f028d24edccdc54eb5545c672b793df36cdf595884bd92d7f8d2c6d3ea&X-Amz-Credential=ASIAJIAWPMGEGLDR7WZQ/20150220/eu-west-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=AQoDYXdzEHEakAJJCBWjcPanmbMG%2BNngj0yTBGvyRLz9gYFFqkAdxs9wEd2ZKPgrjkWTJj/e%2BW0N5PXm6gbkNHKIh3wBBM7yG%2B5ORbr62TqbmbcBNXX40uhfpCJSmOpX%2Bupgtc/IZTJJVZ92QeBwGzlAmPnBkYre7b6Zw4UpAT3/BR0gEYjcUBhGA6XO7cBRtAIUeLh1YbWN1oIFxWp8q2wwpo47XD6ZyJ4E7p12FtFY/o0Sm7EwpwoQLzkP6HCJ1sM%2BGFRQmR2MO6WKRey5LEreoWMKIZD/WRLwycsi7LZ011pDxP6Dx1mgjNEmCqpBT5pMVsYgaqXEHF8JXeEzZiAGOkwLU5tA3Fwsm%2BHNWROVkiFPeWTr4AbwCiC/15unBQ%3D%3D",
-        width: 80,
-        height: 80})
+        url: view_context.image_path("logo.svg"),
+        width: 100,
+        height: 100})
+      marker.title plane.price.to_s
     end
   end
 
